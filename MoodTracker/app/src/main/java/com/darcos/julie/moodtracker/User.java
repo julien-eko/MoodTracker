79f5be39427d46f60eecef48742e2be470f59391
julien.d <julien.darcos@gmail.com>
2018-12-19 14:21:42 +0100

    New class user for information mood day and comment on the mood of the day

---
+++ MoodTracker/app/src/main/java/com/darcos/julie/moodtracker/User.java
@@ -0,0 +1,31 @@
+package com.darcos.julie.moodtracker;
+
+import java.util.ArrayList;
+
+public class User {
+
+    private ArrayList<String> moodComment;
+    private ArrayList<Integer> moodDay;
+
+    public User(){
+        moodComment = new ArrayList();
+        moodDay = new ArrayList();
+
+    }
+
+    public ArrayList<String> getMoodComment() {
+        return moodComment;
+    }
+
+    public void addMoodComment(String Comment) {
+        this.moodComment.add(Comment);
+    }
+
+    public ArrayList<Integer> getMoodDay() {
+        return moodDay;
+    }
+
+    public void addMoodDay(Integer moodDay) {
+        this.moodDay.add(moodDay);
+    }
+}
