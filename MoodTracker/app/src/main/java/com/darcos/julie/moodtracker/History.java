6eb927b7c04e689b16dc7a7e000171df4b1edae6
julien.d <julien.darcos@gmail.com>
2018-12-28 16:06:20 +0100

    iprovement layout history for diferent size

--- MoodTracker/app/src/main/java/com/darcos/julie/moodtracker/History.java
+++ MoodTracker/app/src/main/java/com/darcos/julie/moodtracker/History.java
@@ -3,6 +3,7 @@ package com.darcos.julie.moodtracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -23,6 +24,7 @@ public class History extends AppCompatActivity {
 
         ArrayList<RelativeLayout> listLayout =new ArrayList<RelativeLayout>();
         ArrayList<Button> listButton =new ArrayList<Button>();
+        ArrayList<View> listView =new ArrayList<View>();
 
         RelativeLayout mLayout1 =(RelativeLayout) findViewById(R.id.layout1);
         RelativeLayout mLayout2 =(RelativeLayout) findViewById(R.id.layout2);
@@ -38,27 +40,38 @@ public class History extends AppCompatActivity {
         Button mButton5=(Button) findViewById(R.id.Comment5);
         Button mButton6=(Button) findViewById(R.id.Comment6);
         Button mButton7=(Button) findViewById(R.id.Comment7);
-        listLayout.add(mLayout7);listButton.add(mButton7);
-        listLayout.add(mLayout6);listButton.add(mButton6);
-        listLayout.add(mLayout5);listButton.add(mButton5);
-        listLayout.add(mLayout4);listButton.add(mButton4);
-        listLayout.add(mLayout3);listButton.add(mButton3);
-        listLayout.add(mLayout2);listButton.add(mButton2);
-        listLayout.add(mLayout1);listButton.add(mButton1);
+        View view1 = findViewById(R.id.layout1);
+        View view2 = findViewById(R.id.layout2);
+        View view3 = findViewById(R.id.layout3);
+        View view4 = findViewById(R.id.layout4);
+        View view5 = findViewById(R.id.layout5);
+        View view6 = findViewById(R.id.layout6);
+        View view7 = findViewById(R.id.layout7);
+        listLayout.add(mLayout7);listButton.add(mButton7);listView.add(view7);
+        listLayout.add(mLayout6);listButton.add(mButton6);listView.add(view6);
+        listLayout.add(mLayout5);listButton.add(mButton5);listView.add(view5);
+        listLayout.add(mLayout4);listButton.add(mButton4);listView.add(view4);
+        listLayout.add(mLayout3);listButton.add(mButton3);listView.add(view3);
+        listLayout.add(mLayout2);listButton.add(mButton2);listView.add(view2);
+        listLayout.add(mLayout1);listButton.add(mButton1);listView.add(view1);
 
         //mButton1.setVisibility(View.INVISIBLE);
+        //View view = findViewById(R.id.layout1);
+        //ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        //layoutParams.width = (layoutParams.width)/2 ;
+        //view.setLayoutParams(layoutParams);
+
+        this.layoutConstructor(listLayout,listButton,listView);
 
 
-        this.layoutConstructor(listLayout,listButton);
-
-
-       //Toast.makeText(History.this, this.mComment.get(2), Toast.LENGTH_SHORT).show();
+      // Toast.makeText(History.this, Integer.toString(test2), Toast.LENGTH_SHORT).show();
 
     }
 
 
 
-    public void colorOfLayout(RelativeLayout r1,String mood) {
+    public void colorOfLayout(RelativeLayout r1,String mood,View view) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         if (mood == null) {
 
         } else {
@@ -68,15 +81,24 @@ public class History extends AppCompatActivity {
                     break;
                 case "happy":
                     r1.setBackgroundColor(getResources().getColor(R.color.light_sage));
+                    layoutParams.width = (layoutParams.width)/2 ;
+                    view.setLayoutParams(layoutParams);
                     break;
                 case "normal":
                     r1.setBackgroundColor(getResources().getColor(R.color.cornflower_blue_65));
+                    layoutParams.width = (layoutParams.width)/3 ;
+                    view.setLayoutParams(layoutParams);
                     break;
                 case "disappointed":
                     r1.setBackgroundColor(getResources().getColor(R.color.warm_grey));
+                    layoutParams.width = (layoutParams.width)/4 ;
+                    view.setLayoutParams(layoutParams);
                     break;
                 case "sad":
                     r1.setBackgroundColor(getResources().getColor(R.color.faded_red));
+
+                    layoutParams.width = (layoutParams.width)/5 ;
+                    view.setLayoutParams(layoutParams);
                     break;
                 case "":
                     r1.setBackgroundColor(getResources().getColor(R.color.faded_red));
@@ -102,10 +124,10 @@ public class History extends AppCompatActivity {
 
         }
     }
-    public void layoutConstructor(ArrayList<RelativeLayout> listLayout,ArrayList<Button> listButton){
+    public void layoutConstructor(ArrayList<RelativeLayout> listLayout,ArrayList<Button> listButton,ArrayList<View> listView){
 
         for(int i=1;i<=listLayout.size();i++){
-            colorOfLayout(listLayout.get(i-1),this.mDayMood.get(i));
+            colorOfLayout(listLayout.get(i-1),this.mDayMood.get(i),listView.get(i-1));
             buttonVisibility(listButton.get(i-1),this.mComment.get(i));
         }
     }
